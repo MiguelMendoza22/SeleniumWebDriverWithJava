541aa619c261fcbdfaa6d6ce90c3eada33343af8
Jesus Miguel Molina Mendoza <jesusmiguelmm@ufps.edu.co>
2023-05-31 13:00:42 -0500

    ejercicio 1

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,9 +1,13 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -17,6 +21,15 @@ public class BaseTests {
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
+        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContent.click();
+
+        WebElement menuElement = driver.findElement(By.linkText("Example 1: Menu Element"));
+        menuElement.click();
+
+        List<WebElement> menus = driver.findElements(By.tagName("li"));
+        System.out.println(menus.size());
+
         driver.quit();
     }
 
