cb3bc3f68ead646a3fb9be2fd51d3101b371af66
Jesus Miguel Molina Mendoza <jesusmiguelmm@ufps.edu.co>
2023-05-31 12:20:54 -0500

    se agrega proyecto incial

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,28 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(co);
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+
+}
